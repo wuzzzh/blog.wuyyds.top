9dd617176cb65e8f3b86fbd07b6d6a29c8d6cc0a
wuyyds <wuzzzh@gmail.com>
2024-05-05 17:11:03 +0800

    init

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,31 +1,31 @@
-import { defineConfig } from "vuepress/config";
-import navbar from "./navbar";
-import sidebar from "./sidebar";
-import footer from "./footer";
-import extraSideBar from "./extraSideBar";
+import { defineConfig } from 'vuepress/config';
+import navbar from './navbar';
+import sidebar from './sidebar';
+import footer from './footer';
+import extraSideBar from './extraSideBar';
 
-const author = "程序员山海";
-const domain = "https://codefather.cn";
-const tags = ["程序员", "编程", "计算机"];
+const author = '程序员山海';
+const domain = 'https://wuyyds.top';
+const tags = ['程序员', '编程', '计算机'];
 
 export default defineConfig({
-  title: "山海的秘密花园",
-  description: "贴心的编程学习路线，全面的编程知识百科",
+  title: '山海的秘密花园',
+  description: '贴心的编程学习路线，全面的编程知识百科',
   head: [
     // 站点图标
-    ["link", { rel: "icon", href: "/favicon.ico" }],
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
     // SEO
     [
-      "meta",
+      'meta',
       {
-        name: "keywords",
+        name: 'keywords',
         content:
-          "程序员山海, 编程学习路线, 编程知识百科, Java, 编程导航, 前端, 开发, 编程分享, 项目, IT, 求职, 面经",
+          '程序员山海, 编程学习路线, 编程知识百科, Java, 编程导航, 前端, 开发, 编程分享, 项目, IT, 求职, 面经',
       },
     ],
     // 百度统计
     [
-      "script",
+      'script',
       {},
       `
         var _hmt = _hmt || [];
@@ -38,30 +38,30 @@ export default defineConfig({
       `,
     ],
   ],
-  permalink: "/:slug",
+  permalink: '/:slug',
 
   // 监听文件变化，热更新
-  extraWatchFiles: [".vuepress/*.ts", ".vuepress/sidebars/*.ts"],
+  extraWatchFiles: ['.vuepress/*.ts', '.vuepress/sidebars/*.ts'],
   markdown: {
     // 开启代码块的行号
     lineNumbers: true,
     // 支持 4 级以上的标题渲染
-    extractHeaders: ["h2", "h3", "h4", "h5", "h6"],
+    extractHeaders: ['h2', 'h3', 'h4', 'h5', 'h6'],
   },
   // @ts-ignore
   plugins: [
-    ["@vuepress/back-to-top"],
+    ['@vuepress/back-to-top'],
     // Google 分析
     [
-      "@vuepress/google-analytics",
+      '@vuepress/google-analytics',
       {
-        ga: "GTM-WVS9HM6W", // 补充自己的谷歌分析 ID，比如 UA-00000000-0
+        ga: 'GTM-WVS9HM6W', // 补充自己的谷歌分析 ID，比如 UA-00000000-0
       },
     ],
-    ["@vuepress/medium-zoom"],
+    ['@vuepress/medium-zoom'],
     // https://github.com/lorisleiva/vuepress-plugin-seo
     [
-      "seo",
+      'seo',
       {
         siteTitle: (_, $site) => $site.title,
         title: ($page) => $page.title,
@@ -69,14 +69,14 @@ export default defineConfig({
           $page.frontmatter.description || $page.description,
         author: (_, $site) => $site.themeConfig.author || author,
         tags: ($page) => $page.frontmatter.tags || tags,
-        type: ($page) => "article",
+        type: ($page) => 'article',
         url: (_, $site, path) =>
-          ($site.themeConfig.domain || domain || "") + path,
+          ($site.themeConfig.domain || domain || '') + path,
         image: ($page, $site) =>
           $page.frontmatter.image &&
           (($site.themeConfig.domain &&
-            !$page.frontmatter.image.startsWith("http")) ||
-            "") + $page.frontmatter.image,
+            !$page.frontmatter.image.startsWith('http')) ||
+            '') + $page.frontmatter.image,
         publishedAt: ($page) =>
           $page.frontmatter.date && new Date($page.frontmatter.date),
         modifiedAt: ($page) => $page.lastUpdated && new Date($page.lastUpdated),
@@ -84,25 +84,25 @@ export default defineConfig({
     ],
     // https://github.com/ekoeryanto/vuepress-plugin-sitemap
     [
-      "sitemap",
+      'sitemap',
       {
         hostname: domain,
       },
     ],
     // https://github.com/IOriens/vuepress-plugin-baidu-autopush
-    ["vuepress-plugin-baidu-autopush"],
+    ['vuepress-plugin-baidu-autopush'],
     // https://github.com/zq99299/vuepress-plugin/tree/master/vuepress-plugin-tags
-    ["vuepress-plugin-tags"],
+    ['vuepress-plugin-tags'],
     // https://github.com/znicholasbrown/vuepress-plugin-code-copy
     [
-      "vuepress-plugin-code-copy",
+      'vuepress-plugin-code-copy',
       {
-        successText: "代码已复制",
+        successText: '代码已复制',
       },
     ],
     // https://github.com/webmasterish/vuepress-plugin-feed
     [
-      "feed",
+      'feed',
       {
         canonical_base: domain,
         count: 10000,
@@ -111,22 +111,22 @@ export default defineConfig({
       },
     ],
     // https://github.com/tolking/vuepress-plugin-img-lazy
-    ["img-lazy"],
+    ['img-lazy'],
   ],
   // 主题配置
   themeConfig: {
-    logo: "/logo.png",
+    logo: '/logo.png',
     nav: navbar,
     sidebar,
-    lastUpdated: "最近更新",
+    lastUpdated: '最近更新',
 
     // GitHub 仓库位置
-    repo: "liyupi/codefather",
-    docsBranch: "master",
+    repo: 'wuzzzh/blog.wuyyds.top',
+    docsBranch: 'main',
 
     // 编辑链接
     editLinks: true,
-    editLinkText: "完善页面",
+    editLinkText: '完善页面',
 
     // @ts-ignore
     // 底部版权信息
--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,5 @@
 ---
 home: false
 ---
+
+Hello World
